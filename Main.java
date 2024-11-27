e5bc58b8658d5ee33de298e46fb58a45ad87b61a
Pikaku1 <aliasgar.mullamithawala@gmail.com>
2024-11-27 16:02:55 -0500

    Reset
    Resetting main repo...

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 public class Main{
-  public static void main (String[] args){
-    Addition x = (a, b) -> {
-      int sum = 0;
-      for(int i = a; i <= b; i++) {
-        sum += i;
-      }
-      return sum;
-    };
-    System.out.println(x.add(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
-    //Mohammed Sadiq && Aliasgar Mullamithawala
+  public static void main (String [] args){
+    
   }
 }
